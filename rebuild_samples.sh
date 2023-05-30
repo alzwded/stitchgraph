877d7acab7a43ca1c3f7841b15bcc6baac99a63b
Vlad Meșco <alzwded@gmail.com>
2023-05-30 20:35:39 +0200

    rebuild_samples also rebuilds samples/README

--- rebuild_samples.sh
+++ rebuild_samples.sh
@@ -7,8 +7,21 @@ fail() {
 
 make || fail 'failed to build'
 
-set -x
+rm -f samples/README.md
+touch samples/README.md
+
 for i in samples/*.png ; do
     ./stitchgraph < ${i%.*}
     cp graph.png $i
+    ii=${i##*/}
+    title="#${ii%.*}"
+    title=${title//_/\\_}
+    echo "${title}" >> samples/README.md
+    echo >> samples/README.md
+    echo '```' >> samples/README.md
+    cat ${i%.*} >> samples/README.md
+    echo '```' >> samples/README.md
+    echo >> samples/README.md
+    echo "![${i//_/\\_}](${ii})" >> samples/README.md
+    echo >> samples/README.md
 done
