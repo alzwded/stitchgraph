d6f6e667957edde797cd6785041e4863612f6a4c
Vlad Meșco <alzwded@gmail.com>
2023-05-31 21:03:05 +0300

    eliminate bashism

--- rebuild_samples.sh
+++ rebuild_samples.sh
@@ -15,13 +15,14 @@ for i in samples/*.png ; do
     cp graph.png $i
     ii=${i##*/}
     title="# ${ii%.*}"
-    title=${title//_/\\_}
+    title=$( echo $title | sed -e 's/_/\\_/g' )
     echo "${title}" >> samples/README.md
     echo >> samples/README.md
     echo '```' >> samples/README.md
     cat ${i%.*} >> samples/README.md
     echo '```' >> samples/README.md
     echo >> samples/README.md
-    echo "![${i//_/\\_}](${ii})" >> samples/README.md
+    alt_text=$( echo $i | sed -e 's/_/\\_/g' )
+    echo "![${alt_text}](${ii})" >> samples/README.md
     echo >> samples/README.md
 done
