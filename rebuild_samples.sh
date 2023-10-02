cd4f1f37bb4720869c84b52b24eded513fae121f
Vlad Meșco <alzwded@gmail.com>
2023-10-02 21:26:32 +0300

    fix rebuild_samples.sh

--- rebuild_samples.sh
+++ rebuild_samples.sh
@@ -11,8 +11,7 @@ rm -f samples/README.md
 touch samples/README.md
 
 for i in samples/*.png ; do
-    ./stitchgraph < ${i%.*} || fail $i
-    cp graph.png $i
+    ./stitchgraph ${i%.*} || fail $i
     ii=${i##*/}
     title="# ${ii%.*}"
     title=$( echo $title | sed -e 's/_/\\_/g' )
